3988fb266661008a4e9ba5cf171a000c77e8dd49
Yevhen Varenko <yevhenvarenko@MacBook-Air-Yevhen.local>
2024-03-17 18:23:01 +0100

    solved

--- w1m1t1.py
+++ w1m1t1.py
@@ -1,7 +1,10 @@
+import base64
 # Define an array of strings representing the steps in a software development lifecycle
 steps = ["plan", "code", "test", "delivery", "deploy", "monitor"]
 
 # Loop through each step in the array and perform an operation
 for step in steps:
+    step_bytes = step.encode('utf-8')
+    encoded_step = base64.b64encode(step_bytes)
     # Perform some operation related to the current step
-    print(step)
+    print(encoded_step)
